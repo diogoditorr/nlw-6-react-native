94de7a508e398cd8da8638ce43982344ec172d10
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-04-05 13:08:24 -0300

    [Mission: React Native] Aula 01 - Liftoff

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Image, StatusBar, Text, View } from "react-native";
+import IllustrationImg from "../../assets/illustration.png";
+import ButtonIcon from "../../components/ButtonIcon";
+import { styles } from "./styles";
+export function SignIn() {
+    return (
+        <View style={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon title="Entrar com Discord" activeOpacity={0.75} />
+            </View>
+        </View>
+    );
+}
